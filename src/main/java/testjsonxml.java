1b455e1b05008b484554bcb85423d7a98f510cd3
LakshmiAnushaPinas <117741317+LakshmiAnushaPinas@users.noreply.github.com>
2022-11-14 19:25:12 +0530

    Update testjsonxml.java

--- src/main/java/testjsonxml.java
+++ src/main/java/testjsonxml.java
@@ -1,16 +1,21 @@
 package test.sample.test;
 
 import com.sun.security.jgss.GSSUtil;
+
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
+import javax.xml.namespace.NamespaceContext;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
 import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 
@@ -19,6 +24,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.text.ParseException;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 
@@ -43,26 +49,32 @@ public class testjsonxml {
 		 
 		 //Get DOM 
 		  DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-		  DocumentBuilder db = dbf.newDocumentBuilder(); Document xml =db.parse(xmlFile);
+		  DocumentBuilder db = dbf.newDocumentBuilder();
+		  Document xml =db.parse(xmlFile);
 	
 		  //Get XPath 
-		  XPathFactory xpf = XPathFactory.newInstance(); XPath xpath =
-		  xpf.newXPath();
-		 
-
-    while(keys.hasNext()) {
-        JSONObject values= (JSONObject) object.getJSONObject("fields").get(keys.next());
-        JSONObject locator=  values.getJSONObject("locator");
-        String originalXpath=locator.get("value").toString();
-        System.out.println(locator.get("value"));
-       String name = (String) xpath.evaluate(originalXpath, xml, XPathConstants.STRING);
-			 
-			   if(name.equals("")) {
-			   System.out.println("xpath which don't have value in xml"+ originalXpath);
-			   }//if
-			  
+		
+		  XPathFactory xpf = XPathFactory.newInstance();
+		  XPath xpath = xpf.newXPath();
+		  
+		  JSONObject values=(JSONObject) object.get("fields");
+		 	for(Object key : values.keySet())
+		 	{
+		 		String keyStr=(String)key;
+		 		//System.out.println("field name is "+ keyStr);
+		 		JSONObject myxpaths= (JSONObject) object.getJSONObject("fields").get(keys.next());
+		 	    JSONObject locator=  myxpaths.getJSONObject("locator");
+		 	    String originalXpath=locator.get("value").toString();
+	           // System.out.println(locator.get("value"));
+	            Object name1 =  xpath.evaluate(originalXpath, xml, XPathConstants.BOOLEAN);
+	            boolean validation=(Boolean)name1;
+	            if(validation==false)
+	            {
+	            	System.out.println(keyStr +" " +originalXpath);
+	            }
+    }
+    		  
     }
 
-    }}
-
+    }
 
